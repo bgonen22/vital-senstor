3877b9c7b121f0473c13c61aa7c3b2bb79d618f5
bgonen22 <bgonen22@gmail.com>
2016-08-22 07:40:01 +0300

    Add files via upload

--- vital_sensor.ino
+++ vital_sensor.ino
@@ -11,11 +11,10 @@
 //#define sck 13
 //
 
-//#define timeLimit 10000//milliseconds
-#define speakerPin 9
-//#define delayTime 100//milliseconds
-#define maxBeeps 3000
 
+#define speakerPin 9
+#define maxBeeps 3000
+#define DIST 250
 // Which pin on the Arduino is connected to the NeoPixels?
 #define PIN            6
 
@@ -25,8 +24,8 @@
 int badLoops;
 int goodLoops;
 int beepStart;
-int beepCount;
-int threshold = 10;//(timeLimit / delayTime);
+int threshold = 10;// number of badLoops to start the alarm
+int goodThreshold = 3; // number of samples to reset the badLoops
 
 int iterations_for_avg = 5;
 NewPing sonar(trigPin, echoPin ,300 ) ;
@@ -39,7 +38,6 @@ void unAlert(){
   digitalWrite(speakerPin, LOW);
   badLoops = 0;
   goodLoops = 0;
-  beepCount = 0;
   light(pixels.Color(0,0,0));
   Serial.println("SILENT!");
 }
@@ -51,45 +49,46 @@ void startAlert(){
   unsigned long dist;
  // digitalWrite(speakerPin, LOW);  
   int far;
-  Serial.print("beepCount ");
-  Serial.println(beepCount);
-  while (beepCount <= maxBeeps) {
-    
-    far = 0;
+  int beepCount;
+  far = 0;  
+  beepCount = 0;
+  Serial.println("start aler");
+  while (beepCount <= maxBeeps && far < 3) {
+    Serial.print("beepCount ");
+    Serial.print(beepCount);
+    Serial.print(" far: ");
+    Serial.println(far);
     analogWrite(speakerPin,190);
     light(pixels.Color(0,0,150)); //blue
     delay(1000);
     light(pixels.Color(150,0,0)); //red
     delay(1000); 
     dist = get_dist();   
-    while (dist > 250 || dist < 5) {
+    if (dist > DIST || dist < 5) {
       far++;
       delay(100);
       dist = get_dist();    
       if (far == 3) {
         break;
       }
+    } else {
+      far = 0;    
     }
-    if (far == 3) {
-      break;
-    }
+    beepCount++ ;
    }
    Serial.println("OUT OF ALARM");  
-   beepCount += 1;
+   unAlert();
 }
 // -----------------------------------------------
 // get_dist
 // -----------------------------------------------
 unsigned long get_dist() {
     unsigned long duration,distance;
-    delay(50);    
-     Serial.println(" dist ");
+    delay(50);         
     duration = sonar.ping_median(iterations_for_avg);
-         Serial.println(" dist1 ");
     distance = sonar.convert_cm(duration);
-         Serial.println(" dist 2");
     Serial.print(duration);
-    Serial.print(" dist ");
+    Serial.print(" dist: ");
     Serial.println(distance);
     return distance;
 
@@ -110,9 +109,9 @@ void light (uint32_t color) {
 // -----------------------------------------------
 void setup() {
   badLoops = 0;
-  goodLoops = 0;
-  beepCount = 0;
+  goodLoops = 0;  
   Serial.begin (9600);
+  Serial.println("setup - begin");
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
   pinMode(led, OUTPUT);
@@ -131,29 +130,22 @@ void loop() {
   unsigned long distance;
   distance = get_dist();
 
-  if (distance < 250 && distance > 5) {  // Turn on LED if something is between 5 and 250 cm away from sensor.
+  if (distance < DIST && distance > 5) {  // Turn on LED if something is between 5 and 250 cm away from sensor.
     digitalWrite(led,HIGH);
     badLoops += 1;
-    if(badLoops >= threshold) //if badLoops reaches threshold, turn on alarm
-    Serial.print("start aler");
+    if(badLoops >= threshold) //if badLoops reaches threshold, turn on alarm    
     startAlert();
     goodLoops = 0;
+  } else {
+    goodLoops++;
+    if (goodLoops >= goodThreshold) {
+      badLoops = 0;
+    }
   }
 
-//if goodLoops reaches certain value, turn off alarm and reset badLoops
-  else {
-    goodLoops += 1;
-
-    if(goodLoops >= 5); //set goodloops value (depending on amount of noise)
-      unAlert();
-    digitalWrite(led,LOW);
-  }
    Serial.print("loops: ");
-  Serial.print(goodLoops);
-  Serial.print(" ");
-  Serial.print(badLoops);
-  Serial.println();
-     
+  Serial.println(badLoops);
+       
   delay(1000);
  
 }
